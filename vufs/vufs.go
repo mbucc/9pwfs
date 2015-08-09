648d130dfdb5b66792a919889eb04edbce6bba6a
Mark Bucciarelli <mkbucc@gmail.com>
2015-08-09 00:25:43 -0400

    compile

--- vufs/vufs.go
+++ vufs/vufs.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"github.com/mbucc/vufs"
 	"log"
+	"os"
 )
 
 var addr = flag.String("addr", ":5640", "network address")
@@ -22,7 +23,7 @@ func main() {
 	fs.Id = "vufs"
 	fs.Root = *root
 	fs.Debuglevel = *debug
-	fs.Upool, err  = vufs.NewVusers(root)
+	fs.Upool, err  = vufs.NewVusers(*root)
 	if err != nil {
 		log.Println(err)
 		os.exit(1)
@@ -31,7 +32,7 @@ func main() {
 	fs.Start(fs)
 
 	fmt.Print("vufs starting\n")
-	err := fs.StartNetListener("tcp", *addr)
+	err = fs.StartNetListener("tcp", *addr)
 	if err != nil {
 		log.Println(err)
 		os.exit(1)
